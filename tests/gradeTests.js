e10273652a84823528731bf38cdb209bf6678ad6
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-04-05 11:47:16 +0300

    Test new functionality

--- tests/gradeTests.js
+++ tests/gradeTests.js
@@ -1,9 +1,24 @@
 let gradeBook = require("../lib/grades").gradeBook;
 
+exports["setUp"] = function (callback) {
+    gradeBook.reset();
+    callback();
+}
+
 exports["Can add new grade"] = function (test) {
-    gradeBook.addGrade(75);
+    gradeBook.addGrade(66);
     let count = gradeBook.getGradeCount();
 
     test.equal(count, 1);
     test.done();
-}
+};
+
+exports["Can calculate average"] = function (test) {
+    gradeBook.addGrade(100);
+    gradeBook.addGrade(50);
+
+    let average = gradeBook.getAverage();
+
+    test.equal(average, 75);
+    test.done();
+};
