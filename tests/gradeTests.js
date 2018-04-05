ba8d2c00956816f07cc361a725ed890a105c2102
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-04-05 12:19:46 +0300

    Add getMark() + tests

--- tests/gradeTests.js
+++ tests/gradeTests.js
@@ -22,3 +22,31 @@ exports["Can calculate average"] = function (test) {
     test.equal(average, 75);
     test.done();
 };
+
+exports["Can calculate mark"] = function (test) {
+    gradeBook.addGrade(96);
+    gradeBook.addGrade(89);
+
+    let markA = gradeBook.getMark();
+
+    test.equal(markA, "A");
+    gradeBook.reset();
+
+    gradeBook.addGrade(86);
+    gradeBook.addGrade(81);
+
+    let markB = gradeBook.getMark();
+
+    test.equal(markB, "B");
+    gradeBook.reset();
+
+    gradeBook.addGrade(78);
+    gradeBook.addGrade(63);
+
+    let markC = gradeBook.getMark();
+
+    test.equal(markC, "C");
+    gradeBook.reset();
+
+    test.done();
+};
