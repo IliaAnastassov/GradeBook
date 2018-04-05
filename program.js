da5eda0e8298d5dfc68161dc7606c6a602ea26f6
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-04-05 15:00:56 +0300

    Update getLetterGrade, test & program.js

--- program.js
+++ program.js
@@ -4,8 +4,8 @@ if (process.argv.length > 2) {
     for (let i = 2; i < process.argv.length; i++) {
         gradeBook.addGrade(parseInt(process.argv[i]));
     }
-    
-    console.log(gradeBook.getAverage(), gradeBook.getMark());
+
+    console.log(gradeBook.getAverage(), gradeBook.getLetterGrade());
 } else {
     console.log("Please provide some grades. Ex: [grade1] [grade2] [grade3]")
 }
--- tests/gradeTests.js
+++ tests/gradeTests.js
@@ -23,30 +23,53 @@ exports["Can calculate average"] = function (test) {
     test.done();
 };
 
-exports["Can calculate mark"] = function (test) {
+exports["Can compute letter grade of A"] = function (test) {
     gradeBook.addGrade(96);
     gradeBook.addGrade(89);
 
-    let markA = gradeBook.getMark();
+    let mark = gradeBook.getLetterGrade();
 
-    test.equal(markA, "A");
-    gradeBook.reset();
-
-    gradeBook.addGrade(86);
-    gradeBook.addGrade(81);
-
-    let markB = gradeBook.getMark();
-
-    test.equal(markB, "B");
-    gradeBook.reset();
-
-    gradeBook.addGrade(78);
-    gradeBook.addGrade(63);
-
-    let markC = gradeBook.getMark();
-
-    test.equal(markC, "C");
-    gradeBook.reset();
-
+    test.equal(mark, "A");
     test.done();
 };
+
+exports["Can compute letter grade of B"] = function (test) {
+    gradeBook.addGrade(91);
+    gradeBook.addGrade(83);
+
+    let mark = gradeBook.getLetterGrade();
+
+    test.equal(mark, "B");
+    test.done();
+};
+
+exports["Can compute letter grade of C"] = function (test) {
+    gradeBook.addGrade(79);
+    gradeBook.addGrade(73);
+
+    let mark = gradeBook.getLetterGrade();
+
+    test.equal(mark, "C");
+    test.done();
+};
+
+exports["Can compute letter grade of D"] = function (test) {
+    gradeBook.addGrade(70);
+    gradeBook.addGrade(61);
+
+    let mark = gradeBook.getLetterGrade();
+
+    test.equal(mark, "D");
+    test.done();
+};
+
+exports["Can compute letter grade of F"] = function (test) {
+    gradeBook.addGrade(52);
+    gradeBook.addGrade(45);
+
+    let mark = gradeBook.getLetterGrade();
+
+    test.equal(mark, "F");
+    test.done();
+};
+
